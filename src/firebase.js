da9c807959b910bdcffa7708ca444a87ef2ef2a5
Alireza <alireza27163@gmail.com>
2021-10-03 20:22:47 +0300

    Day 1

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkarPLx89AAb7VEbjfNfbLFsh5U0tBVPk",
+    authDomain: "netflix-clone-df66d.firebaseapp.com",
+    projectId: "netflix-clone-df66d",
+    storageBucket: "netflix-clone-df66d.appspot.com",
+    messagingSenderId: "61118605854",
+    appId: "1:61118605854:web:c87db1787267f9bc5e9c0f",
+    measurementId: "G-XNT8R5EFD6"
+  };
